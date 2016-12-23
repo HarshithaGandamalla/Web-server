142215fdf9e434f1c1243b1a42f36553f5db5bd6
Harshitha Gandamalla <harsh1thagandama@ufl.edu>
2016-12-23 12:45:13 -0600

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,23 @@
+
+
+
+var middleware = {
+ 
+   requireAuthentication: function(req,res,next){
+   	
+   console.log('private route hit!');
+   	next();
+   },
+   logger: function(req,res,next){
+   	
+         	console.log('Request: ' + new Date().toString() + ' ' + req.method +' '+ req.originalUrl);
+   		//	console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+
+   	next();
+
+   }
+ 
+ 	
+};
+
+module.exports=middleware;
