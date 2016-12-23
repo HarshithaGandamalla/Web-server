c2f6c2adad39d8fb523683cfe101f472c652f7ea
Harshitha Gandamalla <harsh1thagandama@ufl.edu>
2016-12-23 11:31:06 -0600

    Initialize repo

---
+++ server.js
@@ -0,0 +1,41 @@
+var express=require('express');
+var app=express();
+var port=3000;
+
+
+var middleware={
+ 
+   requireAuthentication: function(req,res,next){
+   	
+   console.log('private route hit!');
+   	next();
+   },
+   logger: function(req,res,next){
+   	
+         	console.log('Request: ' + new Date().toString() + ' ' + req.method +' '+ req.originalUrl);
+   		//	console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+
+   	next();
+
+   }
+ 
+ 	
+};
+
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication ,function (req,res){
+ 
+ res.send('About Us');
+
+});
+
+app.use(express.static(__dirname+'/public'));
+
+//onsole.log(__dirname);
+
+app.listen(port,function(){
+ 
+ console.log('App started on'+port);
+
+});
